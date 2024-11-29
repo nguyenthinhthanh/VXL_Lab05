e872c3892dfd6b2f7b82c4fd2bae2b1c4f2849cc
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2024-11-29 21:30:27 +0700

    Done test read adc with uart transmit

--- Source/Core/Src/main.c
+++ Source/Core/Src/main.c
@@ -64,13 +64,13 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
 /* USER CODE BEGIN 0 */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 	if(huart->Instance == USART2){
-		//HAL_UART_Transmit(&huart2, &temp, 1, 100);
-		buffer[index_buffer++] = temp;
+		HAL_UART_Transmit(&huart2, &temp, 1, 100);
+		/*buffer[index_buffer++] = temp;
 
 		if(index_buffer >= MAX_BUFFER_SIZE){
 			index_buffer = 0;
 		}
-		buffer_flag = 1;
+		buffer_flag = 1;*/
 
 		HAL_UART_Receive_IT(&huart2, &temp, 1);
 	}
@@ -119,16 +119,16 @@ int main(void)
 
   while (1)
   {
-	  /*HAL_GPIO_TogglePin(LED_PA5_GPIO_Port, LED_PA5_Pin);
+	  HAL_GPIO_TogglePin(LED_PA5_GPIO_Port, LED_PA5_Pin);
 	  ADC_value = HAL_ADC_GetValue(&hadc1);
 	  HAL_UART_Transmit(&huart2, (void *)str, sprintf(str, "%lu\n", ADC_value), 1000);
-	  HAL_Delay(500);*/
+	  HAL_Delay(500);
 
-	  if(buffer_flag == 1){
+	  /*if(buffer_flag == 1){
 		  command_parser_fsm();
 		  buffer_flag = 0;
 	  }
-	  uart_communiation_fsm();
+	  uart_communiation_fsm();*/
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
